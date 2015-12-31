7f37c6eaf6d0ae4ff4d7db6d731ca56ca1cdc7ef
fschwiet <fschwiet@gmail.com>
2015-12-31 06:15:09 -0500

    simplifying nginx config to address weird caching bug

--- host/scripts/map-hostname.sh
+++ host/scripts/map-hostname.sh
@@ -19,10 +19,7 @@ server {
         proxy_pass http://${outgoingHost}:${outgoingPort};
 
         proxy_http_version 1.1;
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection 'upgrade';
         proxy_set_header Host \$host;
-        proxy_cache_bypass \$http_upgrade;
 	}
 }
 "
